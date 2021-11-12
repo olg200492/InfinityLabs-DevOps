3fb84b59c94db84da537b86e000b536c707a3da9
olga.lapovsky <olg200492@gmail.com>
2021-11-12 17:47:54 +0200

    added new test case for StrLen and StrCmp after Arye code reviwe

--- ws2/ex5_test.c
+++ ws2/ex5_test.c
@@ -6,12 +6,43 @@
 int main()
 {
     char str[]= "geeks";
-  
-    printf("Length of string in builtin strlen is: %ld\n", strlen(str));
+    char str2[]= "";
 
-    printf("Length of string in re-implement StrLen is: %ld\n", StrLen(str));
+    char *str1 = "how long is this?";
+	size_t this_long;
+  
+    printf("Olga Lapovsky test case:\n\n");
+    if(strlen(str) == 5)
+    {
+        printf("Test case 1 for StrLen PASSED: \"%s\" string length is 5.\n", str);
+    }
+    else
+    {
+        printf("Test case 1 for StrLen FAILED!!! \n"); 
+    }
+
+    if(strlen(str2) == 0)
+    {
+        printf("Test case 2 for StrLen PASSED: \"%s\" string length is 0.\n", str2);
+    }
+    else
+    {
+        printf("Test case 2 for StrLen FAILED!!! \n"); 
+    }
 
     assert(StrLen(str) == 5);
+
+    printf("/******************************************/\n");
+    printf("Arye Lev Zelkind test case:\n\n\n");
+
+    printf("This is a test for StrLen function on the string:\n\t\t%s\n\n",str1);
+	
+	this_long = StrLen(str1);
+	
+	printf("After using the StrLen function on:\n\t\t%s \nThe calculated value is: %lu\n", str1, this_long );
+	assert(this_long == 17);//aports program if test failed
+	
+	printf("StrLen test successfull!\n");
 
     return (0);
 }
--- ws2/ex6_test.c
+++ ws2/ex6_test.c
@@ -4,13 +4,81 @@
 
 int main()
 {
-    char leftStr[] = "abc";
-    char rightStr[] = "abcd";
+    char leftStr1[] = "abc";
+    char rightStr1[] = "abc";
       
-    int res = StrCmp(leftStr, rightStr);
+    int res1 = StrCmp(leftStr1, rightStr1);
+
+    char leftStr2[] = "ABC";
+    char rightStr2[] = "abc";
+      
+    int res2 = StrCmp(leftStr2, rightStr2);
+
+    char leftStr3[] = "Google";
+    char rightStr3[] = "GooglE";
+      
+    int res3 = StrCmp(leftStr3, rightStr3);
+
+    //Variables for StrCmp Arye Tests
+	char *str2 = "is this the same?";
+	char *str3 = "th thameis is se?";
+	
+	int str_delta;
     
-    assert(res);
-    printf("Strings are unequal\n");         
+    printf("Olga Lapovsky test case:\n\n");
+    if(res1 == 0)
+    {
+        printf("Test case 1 for StrCmp PASSED: \"%s\" string is equel to \"%s\".\n", leftStr1, rightStr1);
+    }
+    else
+    {
+        printf("Test case 1 for StrCmp FAILED!!! \n"); 
+    }
+
+    if(res2 == -32)
+    {
+        printf("Test case 2 for StrCmp PASSED: \"%s\" string is more then to \"%s\".\n", leftStr2, rightStr2);
+    }
+    else
+    {
+        printf("Test case 2 for StrCmp FAILED!!! \n"); 
+    }
+
+    if(res3 == 32)
+    {
+        printf("Test case 3 for StrCmp PASSED: \"%s\" string is less then to \"%s\".\n", leftStr3, rightStr3);
+    }
+    else
+    {
+        printf("Test case 3 for StrCmp FAILED!!! \n"); 
+    }
+    
+
+    /*
+    Arye Lev Zelkind test case
+
+    */ 
+    printf("/******************************************/\n");
+    printf("Arye Lev Zelkind test case:\n\n\n");
+    str_delta = StrCmp(str2, str3);
+	assert(str_delta == -11);
+		
+	printf("calculated value for StrCmp(str2, str3) is: %d \n", str_delta);
+	
+	printf("StrCmp test successfull!\n\nnow testing StrCmp, will compare str 3 and str 2\nstr3 = %s\nstr2 = %s\n", str3, str2);
+	
+	str_delta = StrCmp(str3, str2);
+	assert(str_delta == 11);
+	
+	printf("calculated value for StrCmp(str2, str3) is: %d \n", str_delta);
+	
+	printf("StrCmp test successfull!\n\nnow testing StrCmp, will compare str 2 and str 2\nstr2 = %s\nstr2 = %s\n", str2, str2);
+	
+	str_delta = StrCmp(str2, str2);
+	assert(str_delta == 0);
+	printf("calculated value for StrCmp(str2, str3) is: %d \n", str_delta);
+	
+	printf("StrCmp test successfull! GOOD JOB!!\n");       
 
 
     return (0);
