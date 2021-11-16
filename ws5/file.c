b89091457fe7809cf34131130fb80b35b72b4aeb
olga.lapovsky <olg200492@gmail.com>
2021-11-16 22:42:35 +0200

    ex2 ws5

--- ws5/file.c
+++ ws5/file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h> //strtok
+#include <stdarg.h> // va_list
 #include "file.h"
 
 
@@ -8,7 +9,7 @@ typedef enum op {remove, count, exit, append_beiginning, write} op;
 typedef struct chain{
 
     char *str;
-    void (*Comperision)(char *str);
+    void (*Comperision)(char *str1, char *str2);
     void (*Operation)(int op);
     
 
@@ -50,41 +51,64 @@ void Print(int item)
 int Logger(FILE *f, ...)
 {
     chain arr[4];
+    char file_name[21];
+    char buffer[51];
+    int flag = 0;
+
     arr[0].str = "-remove";
     arr[0].Comperision = Comperision;
     arr[0].Operation = Operation;
 
+    arr[1].str = "-count";
+    arr[1].Comperision = Comperision;
+    arr[1].Operation = Operation;
+
+    arr[2].str = "-exit";
+    arr[2].Comperision = Comperision;
+    arr[2].Operation = Operation;
+
+    arr[3].str = "-exit";
+    arr[3].Comperision = Comperision;
+    arr[3].Operation = Operation;
+
+    printf("Enter name of file:(20 charcter limit\n");
+    scanf("%20s", file_name);
+
+    while (0 == flag)
+    {
+        printf("Enter input:(50 charcter limit\n");
+        scanf("%50s", buffer);
+    }
+    f = fopen()
+ 
+
 }
 
-int Comperision(char string[])
+int Comperision(char string1[], char string2)
 {
-    
-
-    if(strcmp(string,"-remove") == 0)
+    if(strcmp(string1, string2) == 0)
     {
         return (0);
     }
-
-    if(strcmp(string,"-count") == 0)
-    {
-        return (1);        
-    }
-
-    if(strcmp(string,"-exit") == 0)
-    {
-        return (2);        
-    }
-
-    if(strcmp(string[0],"<") == 0)
-    {
-        return (3);        
-    }
-
-    return (-1);
-
+    return (1);
 }
 
 op Operation(int op)
 {
+
     
+}
+
+int CheckBuffer(char buffer[], const chain arr[])
+{
+    int i = 0;
+    int status;
+    for (i = 0; i < 4; i++)
+    {
+        if(arr[i].Comperision(buffer, arr[i].str) == 0)
+        {
+            arr[i].Operation((arr[i].Comperision(buffer, arr[i].str))
+        }
+        
+    }
 }
