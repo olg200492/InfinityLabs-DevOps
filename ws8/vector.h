d269df24af6b5a2a9dfcf72b6f7c3206185de60a
olga.lapovsky <olg200492@gmail.com>
2021-11-23 14:39:36 +0200

    ws8 files

---
+++ ws8/vector.h
@@ -0,0 +1,48 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+#include <stddef.h>
+
+/*
+typedef struct vector vector_t
+{
+    size_t capacity;
+    size_t size;
+    size_t elem_size;
+    void *array;
+}vector_t;
+
+*/
+typedef struct vector vector_t;
+
+/* Creates vector varible ,recives size of element
+    Input:
+    elem_size - Element size
+    capacity - Initial vector size
+    
+    Output:    
+    Address of new vector varible
+
+    Time complexity
+    O(1)
+    */
+vector_t *VectorCreate(size_t elem_size, size_t capacity);
+/* Returns total number of items in vector
+
+ */
+size_t VectorSize(const vector_t *vector);
+/* Resizes vector size (in .c file)
+static void VectorResize(vector_t *vector, int capacity);*/
+/* Adds item at end of array*/
+void VectorAppend(vector_t *vector, const void *item);
+/* Returns reference in index */
+void *VectorGet(const vector_t *vector, int index);
+/* Deletes item at the end of array */
+void VectorPop(vector_t *vector);
+/*Deletes vector data structore */
+void VectorDestroy(vector_t *vector);
+/*Checks if vector is empty,True returns 0, False returns 1 */
+int VectorIsEmpty(const vector_t *vector);
+/*Returns capacity size*/
+size_t VectorCapacity(const vector_t *vector);
+
+#endif
