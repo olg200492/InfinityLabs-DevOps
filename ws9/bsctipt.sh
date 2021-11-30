49dbed4b5d220ee165557a740644145f7fcef261
olga.lapovsky <olg200492@gmail.com>
2021-11-30 08:05:50 +0200

    bash script

--- ws9/bsctipt.sh
+++ ws9/bsctipt.sh
@@ -6,6 +6,27 @@ status="$(systemctl is-active $servicename.service)"
 echo "{$status}"
 echo "Enter 1 for start, 2 for stop,3 for leave it at the current state"
 read num
-if[ num -eq 1 ]
+if [ $num -eq 1 ]
+then
+    if [ $status == "active" ]
     then
-        if[]
+        systemctl restart $servicename service
+    else
+        systemctl start $servicename service
+        status="$(systemctl is-active $servicename.service)"
+        echo "$servicename is now $status"
+    fi
+fi
+if [ $num -eq 2 ]
+then
+    systemctl stop $servicename.service
+    status="$(systemctl is-active $servicename.service)"
+    echo "$servicename is now $status"
+fi
+if [ $num -eq 3 ]
+then 
+    exit 1
+fi
+
+done
+
