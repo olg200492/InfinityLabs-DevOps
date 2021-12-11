01476b7a9f2bd37d3b34ec94fc246c3907abc879
olga.lapovsky <olg200492@gmail.com>
2021-12-11 13:45:46 +0200

    ws15 ex2

---
+++ ws15 python:functional programming/ex2.py
@@ -0,0 +1,2 @@
+def list_comp():
+    return [i if i % 2 == 0 else "odd" for i in range(100)]
--- ws15 python:functional programming/tests.py
+++ ws15 python:functional programming/tests.py
@@ -1,6 +1,7 @@
 from ex1 import sum_list_for
 from ex1 import sum_list_reduce
 from ex1 import sum_list_sum_func
+from ex2 import list_comp
 
 print("Test ex1 sum list in for function")
 print("sum list in for [1,1,1,1] returned:"+str(sum_list_for([1,1,1,1])), " 'PASSED'" if sum_list_for([1,1,1,1]) == 4 else " 'FAILED'")
@@ -15,3 +16,6 @@ print("Test ex1 functions with tuple:")
 print("sum list in for (1,2,3,4) returned:" + str(sum_list_for((1,2,3,4))) )
 print("sum_list_reduce (1,2,3,4) returned:" + str(sum_list_reduce((1,2,3,4))) )
 print("sum_list_sum_func (1,2,3,4) returned:" + str(sum_list_sum_func((1,2,3,4))) )
+print("********************************************")
+print("Test ex2")
+print(list_comp())
