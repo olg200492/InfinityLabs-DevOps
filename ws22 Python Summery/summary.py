3c4a97eddb463eee43aa14cd4982737dc73362e6
olga.lapovsky <olg200492@gmail.com>
2021-12-15 17:47:20 +0200

    4 ex

--- ws22 Python Summery/summary.py
+++ ws22 Python Summery/summary.py
@@ -3,3 +3,28 @@ import os.path
 def return_file_name(path):
     #return path.split("/")[len(path.split("/")) - 1]
     return os.path.basename(path)
+
+def capital_letters_file(path):
+    string = None
+    with open(return_file_name(path),"r") as f:
+        string = f.read().upper()
+    with open(return_file_name(path),"w") as f:
+        f.write(string)
+        
+def check_if_prime(number):
+    if type(number) == float:
+        raise TypeError("TypeError: entered float type")
+    if number > 1 :
+        for i in range( 2, int(number / 2) + 1 ):
+            if number % i == 0:
+                return False
+        return True
+    else:
+        return False
+    
+lambda1 = lambda x:x**2
+
+lambda2 = lambda x, y:x + y
+            
+        
+capital_letters_file("/home/olga/Desktop/git-InfinityLabs/olga-lapovsky/ws22 Python Summery/file.txt")
--- ws22 Python Summery/test_summery.py
+++ ws22 Python Summery/test_summery.py
@@ -1,10 +1,33 @@
 import unittest
-from  summary import return_file_name
+from  summary import return_file_name, check_if_prime, lambda1, lambda2
+
 class SummeryTest(unittest.TestCase):
     
     def test_return_file_name(self):
         self.assertEqual(return_file_name("/home/olga/Desktop/git-InfinityLabs/olga-lapovsky/ws22 Python Summery/file.txt"), "file.txt")
         self.assertEqual(return_file_name("/file.txt"), "file.txt")
+
+    def test_check_if_prime(self):
+        self.assertRaises(TypeError, check_if_prime, "olga")
+        self.assertRaises(TypeError, check_if_prime, 1.22)
+        self.assertEqual(check_if_prime(-100), False)
+        self.assertEqual(check_if_prime(0), False)
+        self.assertEqual(check_if_prime(1), False)
+        self.assertEqual(check_if_prime(2), True)
+        self.assertEqual(check_if_prime(227), True)
+        self.assertEqual(check_if_prime(100), False)
+
+    def test_lambda1(self):
+        self.assertEqual(lambda1(2), 4)
+        self.assertEqual(lambda1(0), 0)
+        self.assertEqual(lambda1(-2), 4)
+        self.assertEqual(lambda1(-3), 9)
+        
+    def test_lambda2(self):
+        self.assertEqual(lambda2(2, 2), 4)
+        self.assertEqual(lambda2(0 ,1), 1)
+        self.assertEqual(lambda2(-2 ,-6), -8)
+        self.assertEqual(lambda2(2.5, 0.5), 3)
 
 if __name__ == '__main__':
     unittest.main()
