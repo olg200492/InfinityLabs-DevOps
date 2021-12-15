29b4267937030534bc44601a5515bc5ce803b5a2
olga.lapovsky <olg200492@gmail.com>
2021-12-15 12:54:23 +0200

    test case for addition

--- ws21 Unittest/test_point.py
+++ ws21 Unittest/test_point.py
@@ -4,17 +4,23 @@ from point import Point, ValueNotNumber
 
 class PointTest(unittest.TestCase):
     def setUp(self):
-        self.p = Point(1, 2)
+        self.p1 = Point(1, 2)
+        self.p2 = Point(3, 4)
     
     def testInit(self):
-        self.assertEqual(self.p.x, 1.0)
-        self.assertEqual(self.p.y, 2.0)
+        self.assertEqual(self.p1.x, 1.0)
+        self.assertEqual(self.p1.y, 2.0)
         self.assertRaises(ValueNotNumber, Point, 1, 'j')
         self.assertRaises(ValueNotNumber, Point, 'k', 'j')
         self.assertRaises(ValueNotNumber, Point, 'k', 2)
     
     def testDistance_from_origin(self):
-        self.assertEqual(self.p.distance_from_origin(), sqrt(5))
+        self.assertEqual(self.p1.distance_from_origin(), sqrt(5))
+    
+    def testAdd(self):
+        p3 = self.p1 + self.p2
+        self.assertEqual(p3.x, 4)
+        self.assertEqual(p3.y, 6)
     
 if __name__ == '__main__':
     unittest.main()
