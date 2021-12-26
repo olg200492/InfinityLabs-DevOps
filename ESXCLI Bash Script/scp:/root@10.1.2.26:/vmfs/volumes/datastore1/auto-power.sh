c8ee96a28061a64032cda5ca6146d2f336a474f2
olga.lapovsky <olg200492@gmail.com>
2021-12-26 09:44:26 +0200

    removed remote folders

--- ESXCLI Bash Script/scp:/root@10.1.2.26:/vmfs/volumes/datastore1/auto-power.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for vmid in $(vim-cmd vmsvc/getallvms | awk '{print $1}')
-do
-    stat1=$(vim-cmd vmsvc/power.getstate "$vmid" | grep “off”)
-    if [ "$stat1" == "Powered off" ]
-        then
-            vim-cmd vmsvc/power.on "$vmid"
-    fi
-done
